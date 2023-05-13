2374ae73fed9654de624b462a6cd62ebd66ed449
Ana Laura Flores <anadisenio@hotmail.com>
2023-05-12 23:47:53 -0300

    add random id and local storage

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ const $$ = (selector) => document.querySelectorAll(selector)
 const showElement = (selector) => $(selector).classList.remove("hidden")
 const hideElement = (selector) => $(selector).classList.add("hidden")
 const cleanContainer = (selector) => $(selector).innerHTML = ""
+// Random id generator
+const randomId = () => self.crypto.randomUUID()
+
+// local storage
+const getData = (key) => JSON.parse(localStorage.getItem(key))
+const setData = (key, array) => localStorage.setItem(key, JSON.stringify(array))
