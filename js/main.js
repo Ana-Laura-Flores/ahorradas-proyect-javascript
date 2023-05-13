a18064e373a31bf1e760848c06c8cbcda86b8d1d
Ana Laura Flores <anadisenio@hotmail.com>
2023-05-12 23:50:26 -0300

    add categories default and get data

--- js/main.js
+++ js/main.js
@@ -12,3 +12,35 @@ const randomId = () => self.crypto.randomUUID()
 // local storage
 const getData = (key) => JSON.parse(localStorage.getItem(key))
 const setData = (key, array) => localStorage.setItem(key, JSON.stringify(array))
+
+// categories default
+const categoriesDefault = [
+    {
+        id: randomId(),
+        categories: "Comida"
+    },
+    {
+        id: randomId(),
+        categories: "Servicios"
+    },
+    {
+        id: randomId(),
+        categories: "Salidas"
+    },
+    {
+        id: randomId(),
+        categories: "Educación"
+    },
+    {
+        id: randomId(),
+        categories: "Transporte"
+    },
+    {
+        id: randomId(),
+        categories: "Trabajo"
+    },
+]
+
+const allOperations = getData("operations") || []
+const allCategories = getData("categories") || categoriesDefault
+
