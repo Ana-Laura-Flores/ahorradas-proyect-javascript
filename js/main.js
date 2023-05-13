5e171fc6b401802ea967f3aa1aadd373ba5572e6
Ana Laura Flores <anadisenio@hotmail.com>
2023-05-12 23:47:01 -0300

    add visibility elements

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
 
+// Visibility elments
+const showElement = (selector) => $(selector).classList.remove("hidden")
+const hideElement = (selector) => $(selector).classList.add("hidden")
+const cleanContainer = (selector) => $(selector).innerHTML = ""
